5873ee19fba7f09a556400cdcf01acf21ec437c2
Tomson Xu <tomson.tao.xu@gmail.com>
2014-11-17 23:12:56 +0800

    add unit test project

---
+++ Lib1Tests/CalculatorCoreTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lib1;
+namespace Lib1Tests
+{
+    [TestClass]
+    public class CalculatorCoreTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CalculatorCore cal = new CalculatorCore();
+            int result = cal.add(1, 2);
+            Assert.AreEqual(5, result);
+        }
+    }
+}
---
+++ Lib1Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lib1Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CityU")]
+[assembly: AssemblyProduct("Lib1Tests")]
+[assembly: AssemblyCopyright("Copyright © CityU 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e12e0202-ba97-487a-86f9-09240f9b626f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
