9243c2756473cdb5a65c4a561365f09f8c76d972
Tomson Xu <tomson.tao.xu@gmail.com>
2014-11-17 22:46:25 +0800

    init version

---
+++ Lib1/CalculatorCore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib1
+{
+    public class CalculatorCore
+    {
+        public int add(int i1, int i2)
+        {
+            return i1 + i2;
+        }
+    }
+}
---
+++ MainApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib1;
+namespace MainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorCore cal = new CalculatorCore();
+            Console.WriteLine(cal.add(1, 2));
+            Console.ReadLine();
+        }
+    }
+}
